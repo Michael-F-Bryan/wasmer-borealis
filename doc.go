b0e7874fbe35ca7b8479a8c8e4e59136f453e159
Michael-F-Bryan <michaelfbryan@gmail.com>
2023-12-17 20:22:21 +0800

    chore: CI will now make sure generated files are always in sync

--- doc.go
+++ doc.go
@@ -1,1 +1,5 @@
 package wasmer_borealis
+
+//go:generate go run github.com/Michael-F-Bryan/wasmer-borealis/cmd/generate-jsonschema
+//go:generate go run github.com/Michael-F-Bryan/wasmer-borealis/cmd/update-graphql-schema
+//go:generate go run github.com/Khan/genqlient@latest
